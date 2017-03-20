3c5aefae09a57b5d8230ce676503a0a15f76c873
Mr.Durden <Mr.Durden@Mr-Durden>
2017-03-20 19:57:34 -0300

    Revert ""
    This reverts commit 2b432a345b7b2b7f5fca5770ebe2205b8dd62a2e.

--- HelloWorld/src/helloworld/HelloWorld.java
+++ HelloWorld/src/helloworld/HelloWorld.java
@@ -2,7 +2,7 @@
 package helloworld;
 public class HelloWorld {
     public static void main(String[] args) {
-      System.out.println("Olá Mundo!!!");
+      System.out.println("Hello World");
     }
     
 }
